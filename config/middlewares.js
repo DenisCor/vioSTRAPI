d443f3e9753578f0ca2be68639fcd0337cead39b
Denis <denis@eedenbull.com>
2022-03-31 19:28:04 +0100

    try the strapi-provider-upload-do package instead

--- config/middlewares.js
+++ config/middlewares.js
@@ -11,8 +11,46 @@
 //   'strapi::public',
 // ];
 
-module.exports = [
+// module.exports = [
+//   "strapi::errors",
+//   {
+//     name: "strapi::security",
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": [
+//             "'self'",
+//             "data:",
+//             "blob:",
+//             "*.digitaloceanspaces.com"
+//           ],
+//           "media-src": ["'self'", "data:", "blob:"],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::logger",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
+
+
+module.exports = ({ env }) => [
   "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::favicon",
+  "strapi::public",
   {
     name: "strapi::security",
     config: {
@@ -24,19 +62,17 @@ module.exports = [
             "'self'",
             "data:",
             "blob:",
-            "*.digitaloceanspaces.com"
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
           ],
-          "media-src": ["'self'", "data:", "blob:"],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
+          ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::favicon",
-  "strapi::public",
 ];
