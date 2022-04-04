a78244b1c27b0817e8bb8d6174fc8c968506914c
Denis <denis@eedenbull.com>
2022-04-04 15:09:53 +0100

    hardcode

---
+++ config/env/production/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: 'TNVWGLDCSMFHNFGLL6D3',
+        secret: 'UnqqnksyeheWKKhhJ+3xTN2btfOHBy//VR5h2RqP4/4',
+        endpoint: 'fra1.digitaloceanspaces.com',
+        space: 'v-space',
+        // directory: "media", // optional
+      },
+    },
+  },
+  // ...
+});
